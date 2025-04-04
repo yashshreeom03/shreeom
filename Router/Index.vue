d8afa2f2a847d1ad8fe5c244c064496da438e187
gajeray5 <153584555+gajeray5@users.noreply.github.com>
2025-04-04 18:13:33 +0530

    update

---
+++ Router/Index.vue
@@ -0,0 +1,29 @@
+<script>
+
+import { createRouter, createWebHistory } from 'vue-router'
+import Home from '~/pages/index.vue'
+import Technology from '~/pages/Technology.vue'
+import Projects from '~/pages/Projects.vue'
+import Company from '~/pages/Company.vue'
+import Career from '~/pages/Career.vue'
+import Contact from '~/pages/Contact.vue'
+
+export default defineNuxtPlugin((nuxtApp) => {
+  const base = import.meta.env.BASE_URL || '/shreeom_website/'
+
+  const router = createRouter({
+    history: createWebHistory(base),
+    routes: [
+      { path: '/', component: Home },
+      { path: '/technology', component: Technology },
+      { path: '/projects', component: Projects },
+      { path: '/company', component: Company },
+      { path: '/career', component: Career },
+      { path: '/contact', component: Contact },
+    ],
+  })
+
+  nuxtApp.vueApp.use(router)
+})
+
+</script>
---
+++ layouts/default.vue
@@ -0,0 +1,26 @@
+<script setup>
+import Header from "~/components/Header.vue";
+import cta from "../components/cta.vue";
+import Footer from "~/components/Footer.vue";
+import Preloader from "@/components/Preloader.vue"
+import ScrollToTopButton from "~/components/ScrollToTopButton.vue";
+</script>
+
+<template>
+  <!-- Header Section -->
+  <Preloader />
+  <Header />
+  <!-- Page Content -->
+  <main>
+    <slot />
+  </main>
+  
+  <!-- Cta Section -->
+  <cta />
+
+  <!-- Footer Section -->
+  <Footer />
+
+  <!-- scroll to top buttton  -->
+  <ScrollToTopButton />
+</template>
